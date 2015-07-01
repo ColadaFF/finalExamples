d0cb42a3a4f5372fb4152db9435ba5287d1fe5c3
Cristian Barrientos Montoya <cbarrientos@ias.com.co>
2015-07-01 00:28:40 -0500

    Final commit for examples

--- www/js/app.js
+++ www/js/app.js
@@ -5,84 +5,165 @@
 // the 2nd parameter is an array of 'requires'
 var finalExamples = angular.module('finalExamples', ['ionic', 'ngCordova']);
 
-finalExamples.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
+finalExamples.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
 });
 
-finalExamples.config(['$ionicConfigProvider', function ($ionicConfigProvider) {
-    $ionicConfigProvider.tabs.position('bottom');
+finalExamples.config(['$ionicConfigProvider', function($ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom');
 }]);
 
-finalExamples.controller('contactController', function ($scope, $cordovaContacts, $ionicModal, $ionicPopup) {
-    $scope.entity = 'Contácto';
-    $scope.model = {};
+finalExamples.controller('contactController', function($scope, $cordovaContacts, $ionicModal, $ionicPopup) {
+  $scope.entity = 'Contï¿½cto';
+  $scope.model = {};
 
-    $ionicModal.fromTemplateUrl('templates/modals/addContact.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-    }).then(function (modal) {
-        $scope.modal = modal;
+  $ionicModal.fromTemplateUrl('templates/modals/addContact.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.getContactList = function() {
+    $cordovaContacts.find({
+      filter: ''
+    }).then(function(result) {
+      console.log(result);
+      console.log(JSON.stringify(result));
+      $scope.contacts = result;
+    }, function(error) {
+      console.log("ERROR: " + error);
     });
-    $scope.openModal = function () {
-        $scope.modal.show();
+  };
+
+  $scope.createContact = function() {
+    var contactModel = {
+      displayName: $scope.model.name,
+      phoneNumbers: [{
+        type: 'mobile',
+        value: $scope.model.tel,
+        pref: true
+      }]
     };
-    $scope.closeModal = function () {
-        $scope.modal.hide();
+    console.log(JSON.stringify(contactModel));
+    $cordovaContacts.save(contactModel).then(function(result) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Genial!',
+        template: 'Has creado un nuevo cont\xe1cto '
+      });
+      alertPopup.then(function(res) {
+        console.log("closed", res);
+      });
+      $scope.closeModal();
+      console.log(JSON.stringify(result));
+    }, function(error) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Error!',
+        template: error
+      });
+      alertPopup.then(function(res) {
+        console.log('Closed!', res);
+      });
+      console.log(error);
+    });
+  };
+
+  $scope.removeContact = function(contact) {
+    $scope.currentContact = contact;
+    $ionicModal.fromTemplateUrl('templates/modals/removeContact.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(removeModal) {
+      $scope.removeModal = removeModal;
+    });
+    $scope.removeModal.show();
+  };
+
+  $scope.closeRemoveModal = function() {
+    $scope.removeModal.hide();
+  };
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+    $scope.removeModal.remove();
+  });
+
+  $scope.removeCurrentContact = function(currentContact) {
+    $cordovaContacts.remove({
+      "id": currentContact.id
+    }).then(function(result) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Genial!',
+        template: 'Has eliminado el cont\xe1cto '
+      });
+      $scope.getContactList();
+      $scope.closeRemoveModal();
+      alertPopup.then(function(res) {
+        console.log("closed", res);
+      });
+    }, function(error) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Error!',
+        template: JSON.stringify(currentContact.id, error)
+      });
+      alertPopup.then(function(res) {
+        console.log('Closed!', res);
+      });
+    });
+  };
+});
+
+finalExamples.controller("cameraController", function($scope, $cordovaCamera, $ionicPopup) {
+  $scope.takePicture = function() {
+    var options = {
+      quality: 75,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 300,
+      targetHeight: 300,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false
     };
 
-    $scope.getContactList = function () {
-        $cordovaContacts.find({filter: ''}).then(function (result) {
-            console.log(result);
-            console.log(JSON.stringify(result));
-            $scope.contacts = result;
-        }, function (error) {
-            console.log("ERROR: " + error);
-        });
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      $scope.imgURI = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+  };
+
+  $scope.pictureURL = function() {
+    var options = {
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: Camera.PictureSourceType.CAMERA,
     };
 
-    $scope.createContact = function () {
-        var contactModel = {
-            displayName: $scope.model.name,
-            phoneNumbers: [
-                {
-                    type: 'mobile',
-                    value: $scope.model.tel,
-                    pref: true
-                }
-            ]
-        };
-        console.log(JSON.stringify(contactModel));
-        $cordovaContacts.save(contactModel).then(function (result) {
-            var alertPopup = $ionicPopup.alert({
-                title: 'Genial!',
-                template: 'Has creado un nuevo cont\xe1cto '
-            });
-            alertPopup.then(function (res) {
-                console.log("closed", res);
-            });
-            $scope.closeModal();
-            console.log(JSON.stringify(result));
-        }, function (error) {
-            var alertPopup = $ionicPopup.alert({
-                title: 'Error!',
-                template: error
-            });
-            alertPopup.then(function (res) {
-                console.log('Closed!', res);
-            });
-            console.log(error);
-        });
-    };
-
-    $scope.removeContact = function () {
-    };
+    $cordovaCamera.getPicture(options).then(function(imageURI) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Exito!!',
+        template: imageURI
+      });
+    }, function(err) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Error!',
+        template: JSON.stringify(err)
+      });
+    });
+  };
 });
